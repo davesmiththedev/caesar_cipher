9e5b885b68001d066c8266bffddbc6f0051824ea
Dave <davesmiththedev@gmail.com>
2019-02-16 11:06:34 -0800

    Refactor variable declarations

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,8 +5,9 @@ get '/' do
   cipher = params['cipher']
   message = params['message']
   decrypt = params[:decrypt]
+  welcome_message = 'Enter a message and a number to seed the encryption cipher.<br><br> Hail Caesar!'
 
-  caesar_says = message && cipher ? 'Caesar says: ' + run_cipher(message, cipher, decrypt) : 'Enter a message and a number to seed the encryption cipher.<br><br> Hail Caesar!'
+  caesar_says = message && cipher ? run_cipher(message, cipher, decrypt) : welcome_message
 
   erb :index, locals: { message: caesar_says }
 end
@@ -14,6 +15,6 @@ end
 def run_cipher(message, cipher, decrypt, alphabet = [*'A'..'Z', *'a'..'z'])
   # Take the negative of the cipher value to decrypt if option selected
   cipher = decrypt ? -cipher.to_i : cipher.to_i
-  # Run encryption using appropriate cipher value
-  message.tr alphabet.join, alphabet.rotate(cipher).join
+  # Run encryption using appropriate cipher value and return the result
+  'Caesar says: ' + (message.tr alphabet.join, alphabet.rotate(cipher).join)
 end
